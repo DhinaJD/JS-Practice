cee565d5fe8bc249499fda33264058af8842528d
Dhina-JD <dhinakaran.jayaraj17@gmail.com>
2026-05-10 14:44:06 +0530

    counter app initial code push

---
+++ counter-app/app.js
@@ -0,0 +1,8 @@
+const increment_btn = document.getElementById("increment-btn");
+const decrement_btn = document.getElementById("decrement-btn");
+let resetBtn = document.getElementById("reset-btn");
+const count = document.getElementById("count");
+
+
+resetBtn.addEventListener("click", ()=>{
+})
