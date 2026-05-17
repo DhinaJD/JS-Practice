0d74567c70424291fa4dd695138b18ef4d06e71d
Dhina-JD <dhinakaran.jayaraj17@gmail.com>
2026-05-17 22:17:41 +0530

    clock date function updates

--- digital-clock-app/app.js
+++ digital-clock-app/app.js
@@ -1,12 +1,30 @@
+/*
+Time Calculation
+
+let minute = 1000 * 60; = 1min
+let hour = minute * 60; = 1hours
+let day = hours * 24; = 1Day
+let year = day * 365 = 1Year
+*/
+
 const clock = document.getElementById("clock");
-const date = document.getElementById("clock");
+const dateText = document.getElementById("date");
 const btn = document.querySelectorAll(".switch-btn");
+
+function timeUpdate(){
+    const currentDate = new Date();
+    let currentTime = `${currentDate.getHours()}: ${currentDate.getMinutes()}: ${currentDate.getSeconds()}`
+
+    dateText.textContent = currentDate.toDateString();
+    clock.textContent = currentTime;
+}
+
+setInterval(() => {
+    timeUpdate();
+}, 1000);
 
 btn.forEach((element) =>{
     element.addEventListener("click", ()=>{
-
-        
-
         btn.forEach(item => {
             item.classList.remove("active");
         });
@@ -14,14 +32,15 @@ btn.forEach((element) =>{
         element.classList.add("active");
 
         //clicking same active button
-        if(element.classList.contains("active")){
-            btn.forEach(item =>{
-                item.classList.add("active");
-            });
-            element.classList.remove("active");
-        }
+        // if(element.classList.contains("active")){
+        //     btn.forEach(item =>{
+        //         item.classList.add("active");
+        //     });
+        //     element.classList.remove("active");
+        // }
     })
 });
+
 
 
 /*
