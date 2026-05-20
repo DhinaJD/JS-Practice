fb25e6a4b14c083acefb8085988e23f7cd9e95c6
Dhina-JD <dhinakaran.jayaraj17@gmail.com>
2026-05-20 08:24:19 +0530

    timer clock completed

--- digital-clock-app/app.js
+++ digital-clock-app/app.js
@@ -10,49 +10,62 @@ let year = day * 365 = 1Year
 const clock = document.getElementById("clock");
 const dateText = document.getElementById("date");
 const btn = document.querySelectorAll(".switch-btn");
-const defaultTimerBtn = document.getElementById("defaultTimer-btn");
-const railwayTimerBtn = document.getElementById("railwaytimer-btn");
+// const defaultTimerBtn = document.getElementById("defaultTimer-btn");
+// const railwayTimerBtn = document.getElementById("railwaytimer-btn");
 let onloadTimer;
 let normalTimerInit;
 
+function timerInit(timeFormat){
 
-let normalTimer = function(){
-    normalTimerInit = setInterval(()=>{
-        console.log("12hr clock Running");
-        const currentDate = new Date();
-        const h = (currentDate.getHours() % 12) < 10 ? `0${currentDate.getHours() % 12}` : currentDate.getHours() % 12 ;
-        const m = currentDate.getMinutes() < 10 ? `0${currentDate.getMinutes()}`: currentDate.getMinutes();
-        const s = currentDate.getSeconds() < 10 ? `0${currentDate.getSeconds()}` : currentDate.getSeconds();
-        let currentTime = `${h}:${m}:${s}`;
-        clock.textContent = currentTime;
-    }, 1000)
+    clearInterval(normalTimerInit);
+    clearInterval(onloadTimer);
+
+    if(timeFormat == "12h"){
+
+        normalTimerInit = setInterval(()=>{
+            console.log("12hr clock Running");
+            const currentDate = new Date();
+            const h = (currentDate.getHours() % 12) < 10 ? `0${currentDate.getHours() % 12}` : currentDate.getHours() % 12 ;
+            const m = currentDate.getMinutes() < 10 ? `0${currentDate.getMinutes()}`: currentDate.getMinutes();
+            const s = currentDate.getSeconds() < 10 ? `0${currentDate.getSeconds()}` : currentDate.getSeconds();
+            let currentTime = `${h}:${m}:${s}`;
+            clock.textContent = currentTime;
+        }, 1000);
+
+        // console.log("12h cliked");
+
+    }else if(timeFormat == "24h"){
+
+        onloadTimer = setInterval(()=>{
+            console.log("24hr clock Running");
+            const currentDate = new Date();
+            let h = currentDate.getHours() > 10 ? currentDate.getHours() : `0${currentDate.getHours()}`;
+            let m = currentDate.getMinutes() > 10 ? currentDate.getMinutes() : `0${currentDate.getMinutes()}`;
+            let s = currentDate.getSeconds() > 10 ? currentDate.getSeconds() : `0${currentDate.getSeconds()}`;
+            let currentTime = `${h}:${m}:${s}`;
+            dateText.textContent = currentDate.toDateString();
+            clock.textContent = currentTime;
+        }, 1000)
+        // console.log("24h clicked");
+
+    }
 }
 
-let railwayTimer = function(){
-    onloadTimer = setInterval(()=>{
-        console.log("24hr clock Running");
-        const currentDate = new Date();
-        let h = currentDate.getHours() > 10 ? currentDate.getHours() : `0${currentDate.getHours()}`;
-        let m = currentDate.getMinutes() > 10 ? currentDate.getMinutes() : `0${currentDate.getMinutes()}`;
-        let s = currentDate.getSeconds() > 10 ? currentDate.getSeconds() : `0${currentDate.getSeconds()}`;
-        let currentTime = `${h}:${m}:${s}`;
-        dateText.textContent = currentDate.toDateString();
-        clock.textContent = currentTime;
-    }, 1000)
-}
+let hourFormat = document.querySelector(".switch-btn.active").getAttribute("data-time");
+timerInit(hourFormat);
 
 //on load clock ticking
-railwayTimer();
+// railwayTimer();
 
-defaultTimerBtn.addEventListener("click", ()=>{
-    clearInterval(onloadTimer);
-    normalTimer();
-});
+// defaultTimerBtn.addEventListener("click", ()=>{
+//     clearInterval(onloadTimer);
+//     normalTimer();
+// });
 
-railwayTimerBtn.addEventListener("click", ()=>{
-    clearInterval(normalTimerInit);
-    railwayTimer();
-});
+// railwayTimerBtn.addEventListener("click", ()=>{
+//     clearInterval(normalTimerInit);
+//     railwayTimer();
+// });
 
 btn.forEach((element) =>{
     element.addEventListener("click", ()=>{
@@ -62,6 +75,9 @@ btn.forEach((element) =>{
         });
 
         element.classList.add("active");
+
+        let hourFormat = element.getAttribute("data-time");
+        timerInit(hourFormat);
 
         // tried setintevel methord inside loop but not working
 
@@ -94,11 +110,21 @@ btn.forEach((element) =>{
 
 
 /*
+
 Nodelist vs htmlcollection 
-getElementsByClassName -  html collection
+getElementsByClassName - html collection
 querySelectorAll - NodeList
 
 contains() - class
 includes() - array
+
+!js is singlethreaded - onecall stack , one execution at time
+
+!callstack - Track of function calls
+
+!const receipt = orderFood(); 
+! foos is already made , receit is just a ticket number same as
+!let  timer = setInterval()
+! it will be automaticaly runs (not inside function)
 
 */
