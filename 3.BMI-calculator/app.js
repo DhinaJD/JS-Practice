e6f3bb1ffcc9318ac47356ec8122be7e228d1179
Dhina-JD <dhinakaran.jayaraj17@gmail.com>
2026-05-22 22:21:21 +0530

    bmi calculation

--- 3.BMI-calculator/app.js
+++ 3.BMI-calculator/app.js
@@ -1,13 +1,38 @@
 const genderBtn = document.querySelectorAll(".gender-btn");
-const bmiInput = document.querySelectorAll("input[name='bmi-input']");
+const bmiInputGroup = document.querySelectorAll("input[name='bmi-input']");
+const userHeight = document.getElementById("input-height");
+const userAge = document.getElementById("input-age");
+const userWeight = document.getElementById("input-weight");
+const bmiResult = document.getElementById("bmi-result");
 
 
+bmiInputGroup.forEach(element => {
+   element.addEventListener("input", (e)=>{
+        let elError = e.target.closest(".input-error");
 
-bmiInput.forEach(element => {
-   element.addEventListener("ch", ()=>{
+        console.log(elError)
 
+        // if(e.target.id == "input-height"){
+        //     if(e.target.value < 50){
+        //         e.target.closest(".input-error").textContent = "hi";
+        //        document.querySelector(".input-error").textContent = "Height should be greater than 50cm"
+        //     }else if(e.target.value > 350){
+        //         "Height should be less than 350 cm"
+        //     }
+        // }
+
+        if(userWeight.value && userAge.value && userHeight.value){
+            //Convert Height cm to meter
+            let newHeight = userHeight.value/100;
+            let newWeight = userWeight.value = userWeight.value.toFixed(2);
+            console.log(newHeight.toFixed(2));
+
+            let result = (newWeight * Math.pow(newHeight.toFixed(2) , 2));
+            bmiResult.textContent = result;
+        }
    });
 });
+
 
 genderBtn.forEach(element => {
     element.addEventListener("click", ()=>{
@@ -18,3 +43,8 @@ genderBtn.forEach(element => {
         element.classList.add("active");
     });
 });
+
+// Height should be greater than 50cm
+// Height should be less than 350 cm
+
+// https://www.axismaxlife.com/insurance-calculators/bmi-calculator
