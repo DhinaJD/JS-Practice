d302aed20ffdcc4b00ed43a8692b6346b521b6f7
Dhina-JD <dhinakaran.jayaraj17@gmail.com>
2026-05-26 21:38:37 +0530

    BMI  calculator

--- 3.BMI-calculator/app.js
+++ 3.BMI-calculator/app.js
@@ -8,10 +8,6 @@ const bmiResult = document.getElementById("bmi-result");
 
 bmiInputGroup.forEach(element => {
    element.addEventListener("input", (e)=>{
-        let elError = e.target.closest(".input-error");
-
-        console.log(elError)
-
         // if(e.target.id == "input-height"){
         //     if(e.target.value < 50){
         //         e.target.closest(".input-error").textContent = "hi";
@@ -20,15 +16,27 @@ bmiInputGroup.forEach(element => {
         //         "Height should be less than 350 cm"
         //     }
         // }
+        
+        if(userWeight.value > 200){
+            let errorMsg = document.createElement("span");
+            errorMsg.classList.add("error-msg");
+            errorMsg.style.color - "red";
+            errorMsg.textContent = "Weight should not be more than 200 kgs"
+            userWeight.append(errorMsg);
+            console.log(errorMsg);
+            
+        }
+
 
         if(userWeight.value && userAge.value && userHeight.value){
+            // 26.81
+            // 84/Math.pow(1.77, 2)
+
             //Convert Height cm to meter
             let newHeight = userHeight.value/100;
-            let newWeight = Number(userWeight.value.toFixed(2))
-            console.log(newHeight);
-
-            let result = (newWeight * Math.pow(newHeight.toFixed(2) , 2));
-            bmiResult.textContent = result;
+            let newWeight = userWeight.value;
+            let result = newWeight / Math.pow(newHeight , 2);
+            bmiResult.textContent = result.toFixed(2);
         }
    });
 });
